c6484f2ac0bb41294ff6c26c5f97837e92db424d
sagorranait <sagorranait@gmail.com>
2024-01-12 07:49:04 +0600

    Design of Details page is done.

---
+++ src/components/VideoDetailSkeleton.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <main>
+    <div class="video-details-skeleton animate-pulse">
+      <div class="w-full h-[500px] bg-slate-200"></div>
+      <div class="mt-5 w-3/4 h-7 bg-slate-200"></div>
+      <div class="my-2 w-1/4 h-5 bg-slate-200"></div>
+      <div class="w-full h-8 bg-slate-200"></div>
+    </div>
+  </main>
+</template>
--- src/views/Details.vue
+++ src/views/Details.vue
@@ -4,6 +4,20 @@
 
 <template>
   <main>
-    <h1>Details</h1>
+    <div class="video-details">
+      <!-- src="https://www.youtube.com/embed/EUZ9FIU4vcI?si=O2RPeYmZmkloWBv1&autoplay=1"  -->
+      <iframe 
+        class="w-full h-[500px]" 
+        src="https://www.youtube.com/embed/EUZ9FIU4vcI?si=O2RPeYmZmkloWBv1" 
+        title="YouTube video player" 
+        frameborder="0" 
+        allow="autoplay; picture-in-picture;" 
+        allowfullscreen
+        autoplay=1
+      ></iframe>
+      <h3 class="mt-5 text-3xl">কার্ড থেকে বিকাশ-এ, টাকা আসবে 'আরামসে' !  - স্পন্সর্ড কন্টেন্ট</h3>
+      <p class="text-base py-2 text-gray-400">LearnwithSumit - 2 months ago</p>
+      <p>কার্ড থেকে বিকাশ-এ, অ্যাড মানি করুন 'আরামসে'! ব্যাংকিং আওয়ার বা বন্ধের দিনের ঝামেলা ছাড়াই কার্ড টু বিকাশ-এর মাধ্যমে একাউন্টে টাকা আনতে পারবেন যখন তখন, যেকোনো সময়ে। পরবর্তী লেনদেনের জন্য কার্ডের তথ্য চাইলে সেভও করে রাখতে পারবেন।</p>
+    </div>
   </main>
 </template>
